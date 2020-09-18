5ac2b6af59f381b6be52af442d11c7e748eafbfe
LaupWing <loc_strict@hotmail.com>
2020-09-18 14:09:47 +0200

    update

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -2,14 +2,18 @@ import {MikroORM} from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from './mikro-orm.config';
+import express from 'express';
+
 
 const main = async () =>{
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
-    // const post = orm.em.create(Post, {title: 'My first post'});
-    // await orm.em.persistAndFlush(post);
-    // const post = await orm.em.find(Post, {});
-    // console.log(post);
+
+    const app = express();
+    app.get('/', (req,res)=>{
+        res.send('Hello World')
+    })
+    app.listen(4000, ()=>console.log(`Server listening on port 4000`))
 };
 
 
