1f49017fb04fde7881d6b52101bb08774536f0a9
LaupWing <loc_strict@hotmail.com>
2020-09-18 14:07:42 +0200

    added working migrations

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -5,9 +5,13 @@ import microConfig from './mikro-orm.config';
 
 const main = async () =>{
     const orm = await MikroORM.init(microConfig);
+    await orm.getMigrator().up();
+    // const post = orm.em.create(Post, {title: 'My first post'});
+    // await orm.em.persistAndFlush(post);
+    // const post = await orm.em.find(Post, {});
+    // console.log(post);
+};
 
-    const post = orm.em.create(Post, {title: 'My first post'});
-    await orm.em.persistAndFlush(post);
-}
 
+main()
 console.log('Hello world');
