59d51da590e834fe6c2cad30b9b28bbdf0e9dc32
LaupWing <loc_strict@hotmail.com>
2020-09-21 18:03:42 +0200

    60

--- lireddit-server/src/resolvers/post.ts
+++ lireddit-server/src/resolvers/post.ts
@@ -1,11 +1,13 @@
 import { Post } from "../entities/Post";
-import { Ctx, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
 import { MyContext } from "../types";
 
 @Resolver()
 export class PostResolver {
-    @Query(()=> [Post])
-    posts(@Ctx() {em}: MyContext): Promise<Post[]>{
-        return em.find(Post, {});
+    @Query(()=> Post, {nullable: true})
+    posts(
+        @Arg('id', ()=> Int) id: number,
+        @Ctx() {em}: MyContext): Promise<Post|null>{
+        return em.findOne(Post, {id});
     }
 }
