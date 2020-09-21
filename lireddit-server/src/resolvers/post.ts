219ae33d762231300e50acb0d14486125e821084
LaupWing <loc_strict@hotmail.com>
2020-09-21 18:18:00 +0200

    createpost

--- lireddit-server/src/resolvers/post.ts
+++ lireddit-server/src/resolvers/post.ts
@@ -1,13 +1,23 @@
 import { Post } from "../entities/Post";
-import { Arg, Ctx, Int, Query, Resolver } from "type-graphql";
+import { Arg, Ctx, Mutation, Query, Resolver } from "type-graphql";
 import { MyContext } from "../types";
 
 @Resolver()
 export class PostResolver {
     @Query(()=> Post, {nullable: true})
     posts(
-        @Arg('id', ()=> Int) id: number,
+        @Arg('id') id: number,
         @Ctx() {em}: MyContext): Promise<Post|null>{
         return em.findOne(Post, {id});
+    }
+
+    @Mutation(()=> Post)
+    async createPost(
+        @Arg('title') title: string,
+        @Ctx() {em}: MyContext
+    ): Promise<Post>{
+        const post = em.create(Post, {title})
+        await em.persistAndFlush(post)
+        return post;
     }
 }
