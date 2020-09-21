70336c3365b537803ecfd48494fd95be53424382
LaupWing <loc_strict@hotmail.com>
2020-09-21 17:34:01 +0200

    57

--- lireddit-server/src/index.ts
+++ lireddit-server/src/index.ts
@@ -1,19 +1,33 @@
+import 'reflect-metadata';
 import {MikroORM} from "@mikro-orm/core";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
+// import { Post } from "./entities/Post";
 import microConfig from './mikro-orm.config';
 import express from 'express';
-
+import {ApolloServer} from 'apollo-server-express';
+import {buildSchema} from 'type-graphql';
+import { PostResolver } from "./resolvers/post";
 
 const main = async () =>{
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
 
     const app = express();
+
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [PostResolver],
+            validate: false
+        }),
+        context: () =>({em: orm.em})
+    });
+
+    apolloServer.applyMiddleware({ app });
+
     app.get('/', (_,res)=>{
         res.send('Hello World')
-    })
-    app.listen(4000, ()=>console.log(`Server listening on port 4000`))
+    });
+    app.listen(4000, ()=>console.log(`Server listening on port 4000`));
 };
 
 
